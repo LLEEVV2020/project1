4d0f4fc326b5fbeda0a466dcc8c0af7980d9d837
sergeyFrontend <utemishevsergey02@gmail.com>
2022-10-26 22:53:11 +0500

    add fancybox

--- gulpfile.js
+++ gulpfile.js
@@ -35,6 +35,7 @@ function styles() {
   return src([
     'node_modules/jquery/dist/jquery.js',
     'node_modules/slick-carousel/slick/slick.js',
+    'node_modules/@fancyapps/ui/src/Fancybox/Fancybox.js',
     'app/js/jquery.maskedinput.js',
     'app/js/main.js'
   ])
